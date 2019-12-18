4376a55eb74b1e617f711e5bd5efe3756683d01a
Wijip <wiji.prabowo0857@gmail.com>
2019-12-18 15:24:56 +0700

    ada error

---
+++ app/src/main/java/com/example/electronic/Splash.java
@@ -0,0 +1,24 @@
+package com.example.electronic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,Welcome.class));
+                finish();
+            }
+        },3000);
+    }
+}
