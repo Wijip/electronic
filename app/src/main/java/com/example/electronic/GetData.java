2f5782d08ec3d4aa0fd506529c818016883c3cb3
Wijip <wiji.prabowo0857@gmail.com>
2019-12-19 10:04:59 +0700

    error GetData

---
+++ app/src/main/java/com/example/electronic/GetData.java
@@ -0,0 +1,122 @@
+package com.example.electronic;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GetData extends Fragment {
+    View myFragment;
+    // [START define_database_reference]
+    private DatabaseReference mDatabase;
+    // [END define_database_reference]
+
+    private FirebaseRecyclerAdapter<Makanan, MakananViewHolder> mAdapter;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private LinearLayoutManager linearLayoutManager;
+
+
+    public GetData() {
+        // Required empty public constructor
+    }
+    public static GetData newInstances()
+    {
+        GetData GetData = new GetData();
+        return GetData;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        myFragment = inflater.inflate(R.layout.activity_main, container, false);
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("electronic");
+
+        recyclerView = myFragment.findViewById(R.id.list_electronic);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(container.getContext(), 1);
+        recyclerView.setLayoutManager(layoutManager);
+
+        Query query = mDatabase;
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<>()
+                .setQuery(query, MainActivity.class)
+                .build();
+
+        mAdapter = new FirebaseRecyclerAdapter<MainActivity, elecViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MakananViewHolder makananViewHolder, int i, @NonNull final Makanan makanan) {
+                makananViewHolder.bindToMakanan(makanan);
+                Glide.with(HomeFragment.this).load(makanan.getGambar()).into(makananViewHolder.ivGambar);
+
+
+                makananViewHolder.cv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Bundle extras = new Bundle();
+
+                        Intent intent = new Intent(view.getContext(), MainActivity.class);
+
+                        extras.putString("" ());
+                        extras.putInt(""());
+                        extras.putString(""());
+
+                        intent.putExtras(extras);
+
+                        startActivity(intent);
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public MakananViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                return new MakananViewHolder(inflater.inflate(R.layout.item_makanan, parent, false));
+            }
+        };
+
+        mAdapter.notifyDataSetChanged();
+        recyclerView.setAdapter(mAdapter);
+
+        if (mAdapter != null) {
+            mAdapter.startListening();
+        }
+
+        System.out.println(mAdapter);
+
+        return myFragment;
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAdapter != null) {
+            mAdapter.stopListening();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/electronic/elecViewHolder.java
@@ -0,0 +1,4 @@
+package com.example.electronic;
+
+public class elecViewHolder {
+}
